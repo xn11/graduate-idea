3fb8c6477c1b65d9f32d1a4e745761698ab68c19
xn11 <xn_28@163.com>
2017-03-20 00:49:21 +0800

    fix getAll(), add Organization, test many2one, delete manageId from Org. and Dept.

---
+++ GagePlatform/src/main/java/com/cebbank/gage/dao/OrganizationDao.java
@@ -0,0 +1,9 @@
+package com.cebbank.gage.dao;
+
+import com.cebbank.gage.model.*;
+
+/**
+ * Created by xn on 2017/3/6.
+ */
+public interface OrganizationDao extends HibernateBaseDao<Organization>{
+}
---
+++ GagePlatform/src/main/java/com/cebbank/gage/service/impl/OrganizationServiceImpl.java
@@ -0,0 +1,42 @@
+package com.cebbank.gage.service.impl;
+
+import com.cebbank.gage.dao.*;
+import com.cebbank.gage.model.*;
+import com.cebbank.gage.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by xn on 2017/3/6.
+ */
+@Service("organizationService")
+public class OrganizationServiceImpl implements OrganizationService {
+
+    @Autowired
+    private OrganizationDao organizationDao;
+
+
+    public void saveList(List<Organization> list) {
+        for (Organization org: list) {
+            organizationDao.save(org);
+        }
+    }
+
+    public List<Organization> getAll() {
+        return organizationDao.getAll();
+    }
+
+    public Organization getById(int id) {
+        return organizationDao.getById(id);
+    }
+
+    public void update(Organization organization) {
+        organizationDao.update(organization);
+    }
+
+    public void delete(int id) {
+        organizationDao.delete(new Organization(id));
+    }
+}
